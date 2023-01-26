e1786a2939aa6b8c74312e90cf4b6943c533a588
eddie0329 <fdsa0329@naver.com>
2023-01-27 00:14:10 +0900

    :sparkles: Implement about page

--- logger/pages/about/index.tsx
+++ logger/pages/about/index.tsx
@@ -1,10 +1,11 @@
+import React, {ReactElement} from 'react'
 import {GetServerSideProps} from 'next'
-import {getServerLogger} from "@/server-logger";
 import {useRouter} from "next/router";
+import {getServerLogger} from "@/server-logger";
 
 export const getServerSideProps: GetServerSideProps = async () => {
     const serverLogger = getServerLogger()
-    serverLogger.log('hello this is home getServerSideProps')
+    serverLogger.log('hello this is about getServerSideProps')
     return {
         props: {
             ...serverLogger.getLoggerProps()
@@ -12,17 +13,15 @@ export const getServerSideProps: GetServerSideProps = async () => {
     }
 }
 
-export default function Home() {
+export default function About(): ReactElement {
     const router = useRouter()
     const handleClick = (): void => {
-        router.push('/about')
+        router.push('/')
     }
     return (
-        <>
-            <div>
-                <h1>Hello Home</h1>
-                <button onClick={handleClick}>GO TO ABOUT</button>
-            </div>
-        </>
+        <div>
+            <h1>hello about</h1>
+            <button onClick={handleClick}>GO TO HOME</button>
+        </div>
     )
 }
